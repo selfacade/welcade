b84f5af5c26473dd390ea29c098dcf600861624d
Mr.JaineekP <jaineekp@gmail.com>
2023-01-14 16:51:10 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
 import Products from "./components/Products/Product";
@@ -7,12 +7,21 @@ import Projects from "./components/Projects/Project";
 
 function App() {
   return (
-    <>
-      <Home />
-      <Services />
-      <Products />
-      <Projects />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Services />
+              <Products />
+              <Projects />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
