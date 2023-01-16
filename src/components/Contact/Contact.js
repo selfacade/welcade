76b292d4414e6ed9036b5320e13f7fcb2ac0dd04
Mr.JaineekP <jaineekp@gmail.com>
2023-01-16 14:21:57 +0530

    complete

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Contact.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Contact() {
+    useEffect(() => {
+        AOS.init();
+      }, []);
   return (
     <main className="contact_container">
        <div className="wrapper">
        <div className="contact_information">
-        <div className="contact_title">
+        <div data-aos="fade-left" className="contact_title">
           Contact <br /> Information
         </div>
         <div className="information">
-          <div className="info"><a href="tel:+9321577721">+91 932-157-7721</a></div>
-          <div className="info"><a href="mailto:welcade.com@gmailcom">welcade.com@gmail.com</a></div>
-          <div className="info"><a href="http://welcade.netlify.app/">http://welcade.netlify.app/</a></div>
+          <div className="info" data-aos="fade-up"><a href="tel:+9321577721">+91 932-157-7721</a></div>
+          <div className="info" data-aos="fade-right"><a href="mailto:welcade.com@gmailcom">welcade.com@gmail.com</a></div>
+          <div className="info" data-aos="fade-left"><a href="http://welcade.netlify.app/">http://welcade.netlify.app/</a></div>
         </div>
       </div>
       <aside className="ContactImage"> 
